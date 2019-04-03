842b87a2e581b73846c005bf85300831f4cdb062
Silvestre Luna <slvtrluna@gmail.com>
2019-04-02 21:28:14 -0500

    completed json

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('hello');
+// XHR - xmlHttpRequest
+let rides = [];
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log(arrayToPrint);
+};
+
+function executeThisCodeAfterFileLoads () {
+    const data = JSON.parse(this.responseText);
+    rides = data.rides;
+    domStringBuilder(data.rides);
+};
+
+function executeThisCodeIfXHRFails(){
+    console.error('oh crap!');
+};
+
+
+const getRidesData = () => {
+    const myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+    myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+    myRequest.open('GET', './db/rides.json');
+    myRequest.send();
+};
+
+
+const init = () => {
+    getRidesData();
+};
+
+init();
